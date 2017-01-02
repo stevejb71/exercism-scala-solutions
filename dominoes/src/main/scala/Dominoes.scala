0bb2cd0d55512a6d6b661ee6542d6044b9298fce
Steve <stevejb71@gmail.com>
2017-01-02 16:49:06 +0800

    outer loop

--- dominoes/src/main/scala/Dominoes.scala
+++ dominoes/src/main/scala/Dominoes.scala
@@ -34,32 +34,38 @@ object Dominoes {
       Some(Nil)
     } else {
       val (g, chain) = findChain(AdjacencyMatrix.ofList(dominoes.tail), List(dominoes.head))
-      var keepGoing = true
-      var extended = chain
       var currGraph = g
       var i = 0
       var chainFound = Seq[(Int, Int)]()
-      @tailrec def findInsertionPoint(i: Int, chainFound: Seq[(Int, Int)], currGraph: AdjacencyMatrix): (Int, Seq[(Int, Int)], AdjacencyMatrix) =  {
+      @tailrec def findInsertionPoint(i: Int, extended: List[(Int, Int)], chainFound: Seq[(Int, Int)], currGraph: AdjacencyMatrix): (Int, Seq[(Int, Int)], AdjacencyMatrix) =  {
         if(i < extended.length && chainFound.size <= 1) {
           val (nextGraph, nextChain) = findChain(currGraph, List(extended(i)))
-          findInsertionPoint(i + 1, nextChain, if (nextChain.size <= 1) currGraph else nextGraph)
+          findInsertionPoint(i + 1, extended, nextChain, if (nextChain.size <= 1) currGraph else nextGraph)
         } else {
           (i, chainFound, currGraph)
         }
       }
-      while(keepGoing) {
-        val innerResult = findInsertionPoint(i, Seq(), currGraph)
+      @tailrec def outer(extended: List[(Int, Int)]): List[(Int, Int)] = {
+        val innerResult = findInsertionPoint(i, extended, Seq(), currGraph)
         i = innerResult._1
         chainFound = innerResult._2
         currGraph = innerResult._3
+        var extended2: List[(Int, Int)] = null
+        var keepGoing = false
         if (chainFound.size > 1) {
           val (before, after) = extended.splitAt(i)
-          extended = before ++ chainFound.tail ++ after
+          extended2 = before ++ chainFound.tail ++ after
           keepGoing = extended.length < dominoes.length
         } else {
           keepGoing = false
         }
+        if(keepGoing) {
+          outer(extended2)
+        } else {
+          extended
+        }
       }
+      val extended = outer(chain)
       if(extended.length == dominoes.length && extended.head._1 == extended.last._2) {
         Some(extended)
       } else {
